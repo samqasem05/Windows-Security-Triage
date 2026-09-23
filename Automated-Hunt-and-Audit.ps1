5b3fbd3d11c701728721b912d4d5b05522e2dfff
osama qasem <166337983+samqasem05@users.noreply.github.com>
2026-09-22 23:42:36 -0700

    Add files via upload

---
+++ Automated-Hunt-and-Audit.ps1
@@ -0,0 +1,26 @@
+﻿# Scenario: "My Internet is Broken & I Can't Print"
+
+$Flushdns = Clear-DnsClientCache
+$ResetAdapter = Restart-NetAdapter -Name "thewifi"
+$TestPrinter = ping 192.168.1.250
+$TestPrinterConnection = Test-NetConnection -port 9100
+$dhcpReset = ipconfig /release 
+$DHcpReset2b= ipconfig /renew
+
+
+
+# Scenario: The "Unauthorized Local Admin" Security Alert
+
+$Getallcurrentusers = Get-LocalUser
+$getUserProvelage = get-localgroupmember -Group administrators
+$disablebackdoor = Disable-LocalUser -Name "backdoor_user"
+$checkforoutboundconn = Get-process -IncludeUserName "backdoor"
+$checkforopenconn = Get-NetTCPConnection -OwningProcess $checkforoutboundconn.name
+
+
+# Scenario: The Persistent "Ghost Process" Ransomware Indicator
+
+$Finmalisousprocess = Get-NetTCPConnection | where-object {$_.RemoteAddress -eq "198.51.100.44"}
+$findtheprocessrelatedtotheip = get-process -id $Finmalisousprocess.OwningProcess -IncludeUserName | Select-Object -Property Name, ID, Path
+$getschaduledtasks = Get-ScheduledTask | where-object {$_.TaskPath -eq $env:TEMP -or $_.TaskPath -eq $env:APPDATA -or$_.TaskPath -eq "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"}
+$NewFireWallRuleToBlock = New-NetFirewallRule -DisplayName Block_Malisious_IP -Description "Malisious IP Address Block Rule" -RemoteAddress 198.51.100.44 -Action Block 
---
+++ unusual.ps1
@@ -0,0 +1,28 @@
+﻿write-host "This scrit will find any malisous services or exes found in unuslau paths, like Temp and Appdata" -ForegroundColor red
+
+$services = Get-CimInstance -ClassName win32_service 
+
+$user = read-host "would u like to begin"
+
+foreach($service in $services){
+
+$name = $service.Name
+
+$path = $service.PathName
+
+if($user -eq "yes"){
+
+
+    $layout = [PSCustomObject]@{
+
+
+    ServiceName = $service.Name
+
+    Servicepath = $service.PathName
+
+    }
+
+}
+
+}
+$layout | convertto-json
