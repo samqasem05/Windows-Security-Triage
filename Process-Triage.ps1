4f6750ee580eeb49166211b4075611b47e8fbe8d
osama qasem <166337983+samqasem05@users.noreply.github.com>
2026-09-22 23:45:49 -0700

    Add Process Triage PowerShell script
    Script retrieves running processes and evaluates their risk levels based on certain criteria.

---
+++ Process-Triage.ps1
@@ -0,0 +1,50 @@
+﻿$processes = Get-CimInstance win32_process | select-object -Property processid, 
+
+parentprocessid, 
+
+processname, 
+
+Executablepath,
+
+commandline
+
+$layout = foreach($proc in $processes){
+
+        $risklevel = "Low"
+
+    if($proc.Executablepath -like "*temp*"){
+
+        write-host "suspisous process" -ForegroundColor Yellow
+
+        $risklevel = "med"
+
+        }
+
+    if($proc.commandline -like "*EncodedCommand*"){
+
+        Write-Host "suspisous command line" -ForegroundColor Red
+
+        $risklevel = "high"
+        }
+
+
+         [PSCustomObject]@{
+
+    Processid = $proc.processid
+
+    parentprocess = $proc.parentprocessid
+
+    processname = $proc.ProcessName
+
+    executablepath = $proc.Executablepath
+
+    commandline = $proc.commandline
+
+    risklevel = $risklevel
+
+    }
+
+}
+
+
+$layout | Out-GridView
